96f1ae7f2c4c516a35f9ae708b51acfc9d3e26b2
Kevin Miller <kjmd10@gmail.com>
2023-08-21 16:15:00 +0100

    Create rnn_utils.py

---
+++ rnn_utils.py
@@ -0,0 +1,77 @@
+
+
+class DatasetRNN():
+  """Holds a dataset for training an RNN, consisting of inputs and targets.
+
+     Both inputs and targets are stored as [timestep, episode, feature]
+     Serves them up in batches
+  """
+
+  def __init__(self,
+               xs: np.ndarray,
+               ys: np.ndarray,
+               batch_size: Optional[int] = None):
+    """Do error checking and bin up the dataset into batches.
+
+    Args:
+      xs: Values to become inputs to the network.
+        Should have dimensionality [timestep, episode, feature]
+      ys: Values to become output targets for the RNN.
+        Should have dimensionality [timestep, episode, feature]
+      batch_size: The size of the batch (number of episodes) to serve up each
+        time next() is called. If not specified, all episodes in the dataset 
+        will be served
+
+    """
+
+    if batch_size is None:
+      batch_size = xs.shape[1]
+
+    # Error checking
+    # Do xs and ys have the same number of timesteps?
+    if xs.shape[0] != ys.shape[0]:
+      msg = ('number of timesteps in xs {} must be equal to number of timesteps'
+             ' in ys {}.')
+      raise ValueError(msg.format(xs.shape[0], ys.shape[0]))
+
+    # Do xs and ys have the same number of episodes?
+    if xs.shape[1] != ys.shape[1]:
+      msg = ('number of timesteps in xs {} must be equal to number of timesteps'
+             ' in ys {}.')
+      raise ValueError(msg.format(xs.shape[0], ys.shape[0]))
+
+    # Is the number of episodes divisible by the batch size?
+    if xs.shape[1] % batch_size != 0:
+      msg = 'dataset size {} must be divisible by batch_size {}.'
+      raise ValueError(msg.format(xs.shape[1], batch_size))
+
+    # Property setting
+    self._xs = xs
+    self._ys = ys
+    self._batch_size = batch_size
+    self._dataset_size = self._xs.shape[1]
+    self._idx = 0
+    self.n_batches = self._dataset_size // self._batch_size
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    """Return a batch of data, including both xs and ys."""
+
+    # Define the chunk we want: from idx to idx + batch_size
+    start = self._idx
+    end = start + self._batch_size
+    # Check that we're not trying to overshoot the size of the dataset
+    assert end <= self._dataset_size
+
+    # Update the index for next time
+    if end == self._dataset_size:
+      self._idx = 0
+    else:
+      self._idx = end
+
+    # Get the chunks of data
+    x, y = self._xs[:, start:end], self._ys[:, start:end]
+
+    return x, y
