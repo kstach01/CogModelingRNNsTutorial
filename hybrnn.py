463eea662d2a3d1a9f1321707dc7e74a03dd470c
MariaEckstein <maria.eckstein@berkeley.edu>
2023-08-22 08:26:32 -0600

    Update hybrnn.py

--- hybrnn.py
+++ hybrnn.py
@@ -1,6 +1,4 @@
 """Define bi-RNNs.
-
-https://stanford.edu/~shervine/teaching/cs-230/cheatsheet-recurrent-neural-networks
 """
 from typing import Optional
 
@@ -18,14 +16,6 @@ class BiRNN(hk.RNNCore):
 
     super().__init__()
 
-    # self._hs = rl_params.hs
-    # self._ho = rl_params.ho
-    # self._vs = rl_params.vs
-    # self._vo = rl_params.vo
-
-    self._zero_values = rl_params.zero_values
-    self._zero_v_states = rl_params.zero_v_states
-
     self._hs = rl_params.s
     self._vs = rl_params.s
     self._ho = rl_params.o
@@ -36,17 +26,9 @@ class BiRNN(hk.RNNCore):
     self._final_activation_fn = network_params.final_activation_fn
     self.beta = rl_params.beta
 
-    if rl_params['fit_init_v']:
-      init = hk.initializers.RandomNormal(stddev=1, mean=1)
-      self._init_value_v = hk.get_parameter('init_value_v', (1,), init=init)
-    else:
-      self._init_value_v = init_value
-
-    if rl_params['fit_init_h']:
-      init = hk.initializers.RandomNormal(stddev=1, mean=1)
-      self._init_value_h = hk.get_parameter('init_value_h', (1,), init=init)
-    else:
-      self._init_value_h = init_value
+    init = hk.initializers.RandomNormal(stddev=1, mean=1)
+    self._init_value_v = hk.get_parameter('init_value_v', (1,), init=init)
+    self._init_value_h = hk.get_parameter('init_value_h', (1,), init=init)
 
     if rl_params['fit_forget']:
       init = hk.initializers.RandomNormal(stddev=1, mean=0)
@@ -56,16 +38,9 @@ class BiRNN(hk.RNNCore):
     else:
       self.forget = rl_params['forget']
 
-    self._w_v = rl_params.w_v
-    self._w_h = rl_params.w_h
-
   def _value_rnn(self, state, value, action, reward):
 
     pre_act_val = jnp.sum(value * action, axis=1)  # (batch_s, 1)
-    if self._zero_values:
-      pre_act_val = jnp.zeros_like(pre_act_val)  # zero input value
-    if self._zero_v_states:
-      state = jnp.zeros_like(state)  # zero input state
 
     inputs = jnp.concatenate(
         [pre_act_val[:, jnp.newaxis], reward[:, jnp.newaxis]], axis=-1)
@@ -77,15 +52,7 @@ class BiRNN(hk.RNNCore):
     next_state = jax.nn.tanh(hk.Linear(self._hidden_size)(inputs))
 
     update = hk.Linear(1)(next_state)
-    # Forget non-chosen values / preferences: decay tow. curr. mean value
-    # value = (1 - self.forget) * value + self.forget * jnp.mean(
-        # value, axis=1, keepdims=True)
-    # Forget non-chosen values / preferences: decay tow. init value
     value = (1 - self.forget) * value + self.forget * self._init_value_v
-    if self._zero_values:
-      next_value = (1 - action) * value + action * update  # zero input value
-    else:
-      next_value = value + action * update
 
     return next_value, next_state
 
@@ -114,7 +81,7 @@ class BiRNN(hk.RNNCore):
     next_habit, next_h_state = self._habit_rnn(h_state, habit, action)
 
     # Combine value and habit
-    hv_combo = self._w_v * next_value + self._w_h * next_habit  # (bs, n_a)
+    hv_combo = next_value + next_habit  # (bs, n_a)
     action_probs = self._final_activation_fn(self.beta * hv_combo)  # (bs, n_a)
 
     return action_probs, (next_h_state, next_v_state, next_habit, next_value)
