e37766db0df921074537b23ff30c2703315e612d
Kim Stachenfeld <kim.stachenfeld@gmail.com>
2023-08-24 20:02:57 +0100

    test

--- cogmodelingrnns/__init__.py
+++ cogmodelingrnns/__init__.py
@@ -1,7 +1,7 @@
 """Initialize."""
-from cogmodelingrnns import bandits
-from cogmodelingrnns import disrnn
-from cogmodelingrnns import hybrnn
-from cogmodelingrnns import rat_data_loader
-from cogmodelingrnns import rnn_utils
+from . import bandits
+from . import disrnn
+from . import hybrnn
+from . import rat_data_loader
+from . import rnn_utils
 
